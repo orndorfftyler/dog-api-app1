985ef98ff94e2abf51d85373f0e09370a132a02b
Tyler Orndorff <orndorfftyler801@gmail.com>
2020-11-08 14:51:46 -0500

    question 2 complete

---
+++ main3.js
@@ -0,0 +1,34 @@
+
+function showDogs(responseJson) {
+    let arr = [];
+    for (let i = 0; i < responseJson.message.length; i++) {
+        arr.push('<img src="'+ responseJson.message[i] + '" width="200px">')
+    }
+    $('div').html(arr.join());
+}
+
+
+function getDogs(dogNum) {
+    //console.log('https://dog.ceo/api/breeds/image/random/'+dogNum);
+    fetch('https://dog.ceo/api/breeds/image/random/'+dogNum)
+    .then(response => response.json())
+    .then(responseJson => showDogs(responseJson))
+    .catch(error => alert('Dogs are lost! Come back later'));
+}
+
+function watchForm() {
+    $('form').on('click', 'button', event => {
+        event.preventDefault();
+        let dogNum = $('input').val();
+        //console.log(dogNum);
+        if (dogNum <= 50) {
+            getDogs(dogNum);
+        }
+    });
+}
+
+
+$(function() {
+    watchForm();
+
+});
