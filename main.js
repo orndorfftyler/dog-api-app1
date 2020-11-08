2ce6035b4211b0b3a089dfdaf17097090df8c11b
Tyler Orndorff <orndorfftyler801@gmail.com>
2020-11-08 14:27:31 -0500

    q1 done

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 
+function showDogs(responseJson) {
+    console.log(responseJson);
+}
 
 
-
-
+function getDogs(dogNum) {
+    //console.log('https://dog.ceo/api/breeds/image/random/'+dogNum);
+    fetch('https://dog.ceo/api/breeds/image/random/'+dogNum)
+    .then(response => response.json())
+    .then(responseJson => showDogs(responseJson))
+    .catch(error => alert('Dogs are lost! Come back later'));
+}
 
 function watchForm() {
-    $('button').on('click', event => {
-    event.preventDefault();
-    
-
-
-    })
+    $('form').on('click', 'button', event => {
+        event.preventDefault();
+        let dogNum = $('input').val();
+        //console.log(dogNum);
+        if (dogNum <= 50) {
+            getDogs(dogNum);
+        }
+    });
 }
 
 
